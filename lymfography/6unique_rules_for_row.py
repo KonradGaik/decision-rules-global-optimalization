750b94898f35ae7ce15a91b3385b1b87951995ed
Konrad <konradgaik@icloud.com>
2024-05-28 12:06:39 +0200

    init

---
+++ lymfography/6unique_rules_for_row.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def count_unique_rules(file_path, inx):
+
+    df = pd.read_csv(file_path)
+    
+
+    def unique_rule_count(matched_rules_str):
+        try:
+
+            rules = matched_rules_str.strip('[]').split(', ')
+
+            unique_rules = set(rules)
+            return len(unique_rules)
+        except Exception as e:
+            print(f"Error parsing rules: {e}")
+            return 0
+
+    df['Unique Rule Count'] = df['Matched Rules'].apply(unique_rule_count)
+    
+    output_file_path = (f'./subtable_{inx}/6before_unique_rule_count_{inx}.csv')
+    df.to_csv(output_file_path, index=False)
+    return output_file_path
+
+for i in range(1, 6):
+    file_path = f'./subtable_{i}/4matched_rows_{i}.csv'
+    output_file = count_unique_rules(file_path, i)
+    print(f"Wynik zapisano do: {output_file}")
---
+++ mushrooms/1make_subtables.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+
+for x in range(1,6):
+    print(x)
+    if not os.path.exists(f'subtable_{x}'):
+        os.makedirs(f'subtable_{x}')
+
+# KROK 1 
+# Utworzenie podtablic z reduktów
+# Wczytaj plik CSV
+df = pd.read_csv('modified_lymphography.csv')
+
+df = df[~df.apply(lambda row: all(value == 'c' for value in row), axis=1)]
+
+
+reducts = [
+    {'block_of_affere', 'changes_in_node', 'changes_in_stru', 'special_forms', 'dislocation_of', 'no_of_nodes_in'},
+    {'block_of_affere', 'defect_in_node', 'changes_in_node', 'changes_in_stru', 'special_forms', 'exclusion_of_no', 'no_of_nodes_in'},
+    {'block_of_affere', 'changes_in_lym', 'defect_in_node', 'changes_in_node', 'changes_in_stru', 'special_forms', 'no_of_nodes_in'},
+    {'block_of_affere', 'early_uptake_in', 'changes_in_lym', 'changes_in_node', 'changes_in_stru', 'special_forms', 'no_of_nodes_in'},
+    {'block_of_affere', 'early_uptake_in', 'changes_in_node', 'changes_in_stru', 'special_forms', 'exclusion_of_no', 'no_of_nodes_in'}
+]
+
+
+for idx, reduct in enumerate(reducts, start=1):
+    df_selected = df[list(reduct) + ['class']]
+    df_selected.to_csv(os.path.join(f'subtable_{idx}', f'1lymphography_reduct_subtable_{idx}.csv'), index=False)
